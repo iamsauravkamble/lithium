07c2fa78a26a19d51f6e05d95fd58606db0f7d57
Saurav Kamble <srvkmbl@gmail.com>
2022-10-19 23:42:17 +0530

    r

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,6 +7,7 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
+
 router.get("/random" , function(req, res) {
     res.send("hi bro")
 })
@@ -116,6 +117,50 @@ let players =
        res.send(  { data: players , status: true }  )
    })
 
+//    Q1.
+   // -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7]
+//    : 4 is missing
+   // Your route code will look like this
+   router.get("/sol1", function (req, res) {
+   //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of
+//    numbers till last digit in the array
+   let arr= [1,2,3,5,6,7]
+   let missingNumber
+   let logic = arr[arr.length-1] * (arr[arr.length-1] + 1) / 2
+//    console.log(logic)
+   let result = 0
+   ///LOGIC WILL GO HERE
+   for(let i=0; i<arr.length; i++){
+     result = result + arr[i]
+   }
+  
+   missingNumber = logic - result
+ 
+   console.log(missingNumber)
+   res.send( { data: missingNumber } );
+   });
+   
+//    Q2.
+   // -write an api which gives the missing number in an array of integers starting from anywhere….e.g [33,
+//    34, 35, 37, 38]: 36 is missing
+   // Your route code will look like this
+   router.get("/sol2", function (req, res) {
+   //logic : sum of n consecutive numbers is [ n * (first + last) / 2 ]..so get sum of all
+//    numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+   let arr= [33, 34, 35, 37, 38]
+   let missingNumber
+   ///LOGIC WILL GO HERE
+   let n = arr.length + 1
+   let first = arr[0]
+   let last = arr[arr.length-1]
+   let result = (n * (first + last) / 2)
+  
+   const total = arr.reduce((acc, curr) => acc + curr)
+   missingNumber = result - total
 
+  console.log(missingNumber)
 
+   res.send( { data: missingNumber } );
+   
+   });
 module.exports = router;
