30c6adb65e1d4b3dd40b35b41991d97c48f6fafe
Saurav Kamble <srvkmbl@gmail.com>
2022-11-06 20:39:56 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://saurav0:NgBLxslFcvt3Zamn@cluster0.7fotmgf.mongodb.net/test", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,4 +1,17 @@
-const authenticate = function(req, req, next) {
+const jwt = require("jsonwebtoken")
+
+const authenticate = function(req, res, next) {
+    let token = req.headers["x-auth-token"]
+    if(!token){
+        return res.send({status:false, msg: "token must be present in the request header"})
+    } 
+
+    let decodeToken = jwt.verify(token, "saurav")
+    if(!decodeToken) {
+        return res.send({status:false, msg:"token is not valid"})
+    } 
+
+    req.userLoggedIn = decodeToken.userId
     //check the token in request header
     //validate this token
 
@@ -7,6 +20,12 @@ const authenticate = function(req, req, next) {
 
 
 const authorise = function(req, res, next) {
+    let userToBeModified = req.params.userId
+    if(userToBeModified !== req.userLoggedIn) return res.send({status:false, msg: "user logged is not allowed to modify the requested users data"})
+
     // comapre the logged in user's id and the id in request
     next()
 }
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
